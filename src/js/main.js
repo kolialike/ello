82b0a1fb857182e352345e3a0bdcfa0b49a4cf2b
kolialike <kolialike@gmail.com>
2016-05-03 16:28:54 +0300

    first-commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,20 @@ jQuery(function($){
   $('.flexslider').flexslider({
     animation: "slide"
   });
+
+  var body = $("body")
+  var bars = $(".bars");
+  bars.on('click', function(event) {
+  	event.preventDefault();
+  	body.toggleClass('mobile-menu-open');
+  });
+  var mobileMenu = $(".header-menu-mobile");
+  mobileMenu.on('click', function(event) {
+  	event.preventDefault();
+  	body.removeClass("mobile-menu-open");
+  });
+  var menuItems = $(".mobile-menu");
+  menuItems.on('click', function(event) {
+  	event.stopPropagation();
+  });
 });
